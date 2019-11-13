29850dc3cc00f49f8dbc5a7fbe8f27bfbfe83dc4
Hao Xu <xuh@cs.unc.edu>
2019-11-13 12:24:35 -0500

    update

--- src/tx/requests/utils.py
+++ src/tx/requests/utils.py
@@ -1,23 +1,6 @@
 from jsonschema import validate
 from oslash import Left, Right
 import requests
-from pint import UnitRegistry
-ureg = UnitRegistry()
-
-def convert(v, u, u2):
-    if u2 is None:
-        return Right(v)
-    else:
-        if u is None:
-            return Left((f"error trying to unitless value {v} without unitful value: {u2}", 403))
-        else:
-            if u == u2:
-                return Right(v)
-            else:
-                try:
-                    return Right((v * ureg.parse_expression(u)).to(ureg.parse_expression(u2)).magnitude)
-                except Exception as e:
-                    return Left(str(e))
 
 
 def request(func, method, url, *args, schema=None, **kwargs):

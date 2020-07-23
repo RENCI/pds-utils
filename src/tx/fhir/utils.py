561fb64b474ffe3e040e2a729b1035be79dd31b9
Hao Xu <xuh@cs.unc.edu>
2020-07-22 21:50:24 -0400

    add bundle type
    add bundle type batch

--- src/tx/fhir/utils.py
+++ src/tx/fhir/utils.py
@@ -4,18 +4,23 @@ def isBundle(bundle):
     return isinstance(bundle, dict) and "resourceType" in bundle and bundle["resourceType"] == "Bundle"
 
     
-def bundle(records):
+def bundle(records, type = "searchset"):
+    key = "request" if type == "batch" else "resource"
     return {
         "resourceType": "Bundle",
+        "type": type,
         "entry": list(map(lambda record: {
-            "resource": record
+            key: record
         }, records))
     }
 
 
 def unbundle(bundle):
     if isBundle(bundle):
-        return Right(list(map(lambda a : a["resource"], bundle.get("entry", []))))
+        if bundle["type"] == "batch":
+            return Right(list(map(lambda a : a["request"], bundle.get("entry", []))))
+        else:
+            return Right(list(map(lambda a : a["resource"], bundle.get("entry", []))))
     else:
         return Left(str(bundle) + " is not a bundle")
 

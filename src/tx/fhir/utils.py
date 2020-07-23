a8de975a336339d800e55361c2ce31f9cf55b1a4
Hao Xu <xuh@cs.unc.edu>
2020-07-23 13:15:32 -0400

    type name

--- src/tx/fhir/utils.py
+++ src/tx/fhir/utils.py
@@ -3,24 +3,27 @@ from tx.functional.either import Left, Right
 def isBundle(bundle):
     return isinstance(bundle, dict) and "resourceType" in bundle and bundle["resourceType"] == "Bundle"
 
-    
+
+def type_name(type):
+    if type == "batch":
+        return "request"
+    else:
+        return "resource"
+
+
 def bundle(records, type = "searchset"):
-    key = "request" if type == "batch" else "resource"
     return {
         "resourceType": "Bundle",
         "type": type,
         "entry": list(map(lambda record: {
-            key: record
+            type_name(type): record
         }, records))
     }
 
 
 def unbundle(bundle):
     if isBundle(bundle):
-        if bundle["type"] == "batch":
-            return Right(list(map(lambda a : a["request"], bundle.get("entry", []))))
-        else:
-            return Right(list(map(lambda a : a["resource"], bundle.get("entry", []))))
+        return Right(list(map(lambda a : a[type_name(bundle["type"])], bundle.get("entry", []))))
     else:
         return Left(str(bundle) + " is not a bundle")
 

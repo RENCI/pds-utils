a3712088253bdda844077269d5c21b72e58ed582
Hao Xu <xuh@cs.unc.edu>
2020-07-27 15:33:47 -0400

    avoid duplicate handlers

--- src/tx/logging/utils.py
+++ src/tx/logging/utils.py
@@ -6,13 +6,14 @@ import logging
 
 def getLogger(name, level):
     logger = logging.getLogger(name)
-    ll = os.environ.get("LOG_LEVEL", level)
-    logger.setLevel(ll)
-    ch = logging.StreamHandler()
-    ch.setLevel(ll)
-    formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    if len(logger.handlers) == 0:
+        ll = os.environ.get("LOG_LEVEL", level)
+        logger.setLevel(ll)
+        ch = logging.StreamHandler()
+        ch.setLevel(ll)
+        formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
     return logger
 
 
